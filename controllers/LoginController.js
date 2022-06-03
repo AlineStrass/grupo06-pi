f573bd080c3352e089c525ae54692f22def39b44
AlineStrass <flores.aline.89@gmail.com>
2022-06-03 20:10:33 -0300

    alteraçoes loginController - valida login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,29 +1,40 @@
 const User = require('../database/models/User');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+const req = require("express/lib/request");
 
 const LoginController = {
     index: (req, res) => {
         return res.render('login')
     },
 
-    acaoLogin: (req, res) => {
-        //console.log('entrou no login')
-        const emailLogin = req.body.email;
-        //console.log(emailLogin)
-
-        if(User.findUserByField('email', emailLogin)){
-            req.session.logado = true;
-            //console.log("teste:")
-            //console.log(req.session.logado)
-            
-            res.redirect('/painelUsuario');
-        }
-        else{
-            res.redirect('/criarConta');
-        }
-
-    }
+    acaoLogin: async (req, res) => {
+        const usuarioLogin = await User.findUserByField({where: {usuario: req.body.email}});
+        console.log('entrou no usuario login')
+        console.log(usuarioLogin)
+        if(usuarioLogin === undefined) {
+            res.redirect('login');
+        }else{
+            const senha = usuarioLogin.senha;
+            const validacaoSenha = (req.body.senha, senha);
+            if(!validacaoSenha){
+                res.render('login');
+            }else{
+                req.session.usuarioLogin = true;
+                console.log("teste: session")
+                console.log(req.session.usuarioLogin)
+                res.render('painelUsuario')
+            }
     
+        }
+
+    },
+/*
+    logout: (req, res) => {
+        req.session.destroy();
+        res.redirect("/");
+    }
+    */
 }
 
 
