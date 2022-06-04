8e5b7019b50210f915e05b034a076dc55d43b985
AlineStrass <flores.aline.89@gmail.com>
2022-06-04 11:42:25 -0300

    criação página not-found

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,28 +1,17 @@
 const User = require('../database/models/User');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
-const req = require("express/lib/request");
-const clientes = require('../database/seeders/20220604000738-clientes');
+const fs = require('fs');
+
+
 
 const LoginController = {
     index: (req, res) => {
         return res.render('login')
     },
     acaoLogin: (req, res) => {
-        //console.log('entrou no login')
-        const emailLogin = req.body.email;
-        //console.log(emailLogin)
-
-        if(User.findUserByField('email', emailLogin)){
-            req.session.logado = true;
-            //console.log("teste:")
-            //console.log(req.session.logado)
-            
-            res.redirect('/painelUsuario');
-        }
-        else{
-            res.redirect('/criarConta');
-        }
+      let {email, senha} = req.body
+      let usuarioSalvo = fs.readFileSync
 
     }
 
