2c36067782eed645ba3921d83c5971d605456134
AlineStrass <flores.aline.89@gmail.com>
2022-06-05 22:32:39 -0300

    implementação de um modelo/teste api em admin

---
+++ controllers/ApiController.js
@@ -0,0 +1,50 @@
+const db = require('../database/models/Api');
+
+//não está funcionando, erro não encontra os métodos fundAll, findByPk, create e delete
+
+let ApiController = {
+    getProdutos: function(req, res) {
+        return res.json('oi')
+    },
+
+    getAdmins: function(req, res) {
+        db.Admin
+        .findAll()
+        .then(retornoAdmin => {
+            console.log('entrou no then')
+            return res.status(200).json(retornoAdmin)
+            console.log(retornoAdmin)
+        })
+    },
+
+    getAdmin: function(req, res) {
+        db.Admin
+        .findByPk(req.params.id)
+        .then(OneUserAdmin => {
+            return res.status(200).json(OneUserAdmin)
+        })
+    },
+
+    createAdmin: function(req, res) {
+        db.Admin
+        .create(req.body)
+        .then(newAdmin => {
+            return res.status(200).json(newAdmin)
+        })
+    },
+    deleteAdmin: function(req, res) {
+        db.Admin
+        .destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(deleteAdmin => {
+            return res.json(deleteAdmin)
+        })
+    }
+
+}
+
+
+module.exports = ApiController;
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var apiController = require('../controllers/apiController');
+var apiController = require('../controllers/ApiController');
+
 
 // lista de produtos
-router.get('/', apiController.getProdutos);
+router.get('/produtos', apiController.getProdutos);
+router.get('/admin', apiController.getAdmins);
+router.get('/admin/:id', apiController.getAdmin);
+router.post('/admin', apiController.createAdmin);
+router.delete('/admin/:id', apiController.deleteAdmin);
+
 
 module.exports = router;
