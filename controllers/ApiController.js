b7b4a22010c9450a188a12854107d5d367c982c0
AlineStrass <flores.aline.89@gmail.com>
2022-06-06 19:10:54 -0300

    testes de API

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -1,6 +1,6 @@
 const db = require('../database/models/Api');
 
-//não está funcionando, erro não encontra os métodos fundAll, findByPk, create e delete
+//não está funcionando, erro não encontra os métodos fndAll, findByPk, create e delete
 
 let ApiController = {
     getProdutos: function(req, res) {
