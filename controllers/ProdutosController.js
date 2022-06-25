e19d45007a361a0b51b259b4cae91f8ac8ee9f63
AlineStrass <flores.aline.89@gmail.com>
2022-06-25 11:29:12 -0300

    ajustes de pastas de imagens

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -2,7 +2,9 @@ const db = require('../database/models');
 
 const ProdutosController = {
     index: async (req, res) => {
-        const produtos = await Produto.findAll
+        const produtos = await db.Produto.findAll({
+            
+        })
 
         return res.render('produtos')
     }
