ab4c81d53f7506f5d1eabb93181d806434d88dee
AlineStrass <flores.aline.89@gmail.com>
2022-06-25 21:08:03 -0300

    mostrar produtos na view via banco de dados

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,9 +1,16 @@
+const sequelize = require('sequelize');
+const config = require('../database/config/config');
 const db = require('../database/models'); 
 
 const HomeController = {
-    index: (req, res) => {
-        return res.render('home')
+    index: async (req, res) => {
+            const produtosHome = await db.Produto.findAll({
+                include: ['categoria']
+            })
+            console.log("produtosHome")
+            return res.render('home', {Produto: produtosHome})
     }
 }
+
 
 module.exports = HomeController;
