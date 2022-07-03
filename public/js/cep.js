4ff85fe41918e2150848803c3349a860b78ee7a2
Pablo <blopa741@gmail.com>
2022-07-03 12:47:31 -0300

    adicao da api de cep na pagina de cadastro

---
+++ public/js/cep.js
@@ -0,0 +1,19 @@
+ var campoCep = document.getElementById("cep");
+
+campoCep.onblur = function (){
+    var valorCep = this.value;
+
+    fetch("https://brasilapi.com.br/api/cep/v1/" + valorCep)
+    .then(async function(resposta){
+        var dados = await resposta.json();  
+        var campoEndereco = document.getElementById("endereco");
+        var campoBairro = document.getElementById("bairro");
+        var campoCidade = document.getElementById("cidade");
+        var campoEstado = document.getElementById("estado");
+
+        campoEndereco.value = dados.street;
+        campoBairro.value = dados.neighborhood;
+        campoCidade.value = dados.city;
+        campoEstado.value = dados.state;
+    });
+}
