25a7fbff18bb23ded660697c50a29a9bfdea7e7b
AlineStrass <flores.aline.89@gmail.com>
2022-07-25 18:59:09 -0300

    msgde erro login admin

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const config = require('../database/config/config');
-const db = require('../database/models'); 
+const db = require('../database/models');
 const bcrypt = require('bcryptjs');
 
 
@@ -9,35 +9,37 @@ const AdminController = {
     index: async (req, res) => {
         if (req.session.logado === true) {
             const detalhesAdmin = await db.Admin.findByPk(req.session.idUsuario)
-            return res.render('admin/admin', {Admin: detalhesAdmin})
+            return res.render('admin/admin', { Admin: detalhesAdmin })
         } else {
             res.render('admin/adminLogin')
         }
     },
-    
+
     adminLogin: (req, res) => {
         return res.render('admin/adminLogin')
     },
 
     //login
     acaoLoginAdmin: async (req, res) => {
-        const {username, senha} = req.body;
+        const { username, senha } = req.body;
         const adminEncontrado = await db.Admin.findOne({
-            where: {username: username}
+            where: { username: username }
         })
         if (adminEncontrado != null) {
             let sucessoSenha = bcrypt.compareSync(senha, adminEncontrado.senha);
-            
+
             if (sucessoSenha) {
                 req.session.logado = true;
                 req.session.idUsuario = adminEncontrado.id;
                 res.redirect('/admin');
 
             } else {
-                res.redirect('/login');
+                res.render('admin/adminLogin', { error: ['Usuário e/ou senha não encontrado']});
+                return;
             }
-        }else{
-            res.redirect('/login');
+        } else {
+            res.render('admin/adminLogin', { error:['Usuário e/ou senha não encontrado']});
+            return;
         }
     },
 
@@ -59,68 +61,68 @@ const AdminController = {
 
     //      ------ USUARIOS ------
     //renderiza a pg principal dos usuarios
-    adminUsuarios: async (req, res) => { 
+    adminUsuarios: async (req, res) => {
         const adminUsers = await db.Admin.findAll();
- 
-        return res.render('admin/adminUsuarios', {Admin: adminUsers})
+
+        return res.render('admin/adminUsuarios', { Admin: adminUsers })
     },
     //renderiza a página do formulario para adicionar usuarios
-    adminUsuariosCadastrar: (req, res) => { 
+    adminUsuariosCadastrar: (req, res) => {
         return res.render('admin/adminUsuariosCadastrar')
     },
-        
+
     //cadastra novos usuarios Administrativos - ok
     acaoCadastrarAdmin: async (req, res) => {
         const cadastrarAdmin = {
             username: req.body.username,
-            senha: bcrypt.hashSync(req.body.senha),  
+            senha: bcrypt.hashSync(req.body.senha),
         }
         await db.Admin.create(cadastrarAdmin)
         res.redirect('/admin/usuarios')
     },
 
     //editar usuarios - ok
-    editarAdmin: async (req, res) =>{
-        const {id} = req.params;
+    editarAdmin: async (req, res) => {
+        const { id } = req.params;
         const usuario = await db.Admin.findByPk(id);
-        res.render('admin/editarAdmin', {Admin: usuario})
+        res.render('admin/editarAdmin', { Admin: usuario })
     },
 
     acaoEditarAdmin: async (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         // console.log("aqui o id de açãoEditarAdmin",id)
-        const {username, senha} = req.body;
+        const { username, senha } = req.body;
         // console.log("aqui o username de acaoEditarAdmin",username)
         // console.log("aqui req.body:", req.body)
         const resultado = await db.Admin.update({
             username,
             senha: bcrypt.hashSync(req.body.senha)
         },
-        {
-            where:{id: id}
-        })
+            {
+                where: { id: id }
+            })
         // console.log(resultado)
         // mostra [1] para ok e [0] para erro
         res.redirect('/admin/usuarios')
     },
 
     deletarAdmin: async (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         const resultado = await db.Admin.destroy({
-            where:{id: id}
+            where: { id: id }
         })
         console.log(resultado)
         res.redirect('/admin/usuarios')
     },
- 
+
 
     //  --------- PRODUTOS -----------
     //renderiza a pg principal da lista de produtos
     adminProdutos: async (req, res) => {
-        const adminProdutos = await db.Produto.findAll({include: ['categoria']});
+        const adminProdutos = await db.Produto.findAll({ include: ['categoria'] });
         const categoria = await db.Categoria.findAll();
         // const imagens = await db.fotoProduto.findAll();
-       
+
         return res.render('admin/adminProdutos', {
             Produto: adminProdutos,
             Categoria: categoria,
@@ -133,14 +135,14 @@ const AdminController = {
         const produto = await db.Produto.findAll();
         const categoria = await db.Categoria.findAll();
         // const imagens = await db.ImagemProduto.findAll();
-            
-        return res.render('admin/adminProdutosCadastrar',{
+
+        return res.render('admin/adminProdutosCadastrar', {
             Produto: produto,
-            Categoria:categoria,
+            Categoria: categoria,
             // fotoProduto: imagens,
         })
     },
-    
+
     //cadastra novos produtos
     acaoCadastrarProdutos: async (req, res) => {
         const cadastrarProdutos = {
@@ -159,7 +161,7 @@ const AdminController = {
         }
         console.log("foto do produto:", cadastrarProdutos.foto)
         // const categoria = await db.Categoria.findAll();
-        await db.Produto.create(cadastrarProdutos, 
+        await db.Produto.create(cadastrarProdutos,
             { include: ["categoria"] })
         // console.log(cadastrarProdutos)
         res.redirect('/admin/produtos')
@@ -167,22 +169,22 @@ const AdminController = {
     },
 
     editarProduto: async (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         const resultado = await db.Produto.findByPk(id);
         const categoria = await db.Categoria.findAll();
-        
+
         // console.log("id:", id)
         // console.log("produtos:", resultado)
         res.render('admin/editarProdutos', {
             Produto: resultado,
-            Categoria:categoria,
-            
+            Categoria: categoria,
+
         })
     },
 
     acaoEditarProduto: async (req, res) => {
-        const {id} = req.params;
-        const {nome, preco, descricao,categoria, foto} = req.body;
+        const { id } = req.params;
+        const { nome, preco, descricao, categoria, foto } = req.body;
         // console.log("dados produto", nome, preco, descricao,categoria, foto)
         const resultado = await db.Produto.update({
             nome,
@@ -191,9 +193,9 @@ const AdminController = {
             foto: req.file.filename,
             categoria,
         },
-        {
-            where:{id: id}
-        })
+            {
+                where: { id: id }
+            })
         console.log(resultado)
         // mostra [1] para ok e [0] para erro
         res.redirect('/admin/produtos')
@@ -201,15 +203,15 @@ const AdminController = {
 
     // excluir produto - ok
     deletarProduto: async (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         const resultado = await db.Produto.destroy({
-            where:{id: id}
+            where: { id: id }
         })
         console.log(resultado)
         res.redirect('/admin/produtos')
     },
 
-  
+
 }
 
 module.exports = AdminController;
