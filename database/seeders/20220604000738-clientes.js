629ed3216af0cde21deebdeda6d64212425390e3
Beatrice-mathey <beatricemathey@gmail.com>
2022-08-01 18:11:45 -0300

    ajustes admin usuarios e produtos

--- database/seeders/20220604000738-clientes.js
+++ database/seeders/20220604000738-clientes.js
@@ -3,13 +3,12 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     // PARA RODAR NO TERMINAL: SEQUELIZE DB:SEED:ALL
-    await queryInterface.bulkInsert('cliente', [
+    await queryInterface.bulkInsert('clientes', [
       {
         id: 1,
         email: "meuemail@email.com",
         telefone: "156654824574",
-        nome: "Fulano",
-        sobrenome: "de Tal",
+        nomeCompleto: "Fulano de tal",
         endereco: "rua dos bobos",
         numero: "4455",
         complemento: "135153",
@@ -23,7 +22,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('cliente', null, {});
+    await queryInterface.bulkDelete('clientes', null, {});
 
   }
 };
