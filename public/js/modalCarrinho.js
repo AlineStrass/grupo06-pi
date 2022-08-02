13bb45fca82b1c7c291848e2443c43900792368b
AlineStrass <flores.aline.89@gmail.com>
2022-08-02 19:18:17 -0300

    ajsutes css carrinho e modal

--- public/js/modalCarrinho.js
+++ public/js/modalCarrinho.js
@@ -3,13 +3,8 @@ function acaoModal(){
 
     msgModal.innerHTML = "Produto adicionado ao carrinho"
     msgModal.style.display = 'block';
-    // msgModal.style.transitionDuration = "2s";
+    setTimeout(function(){
+        msgModal.style.display = "none";
+    },2000)
+
 }
-
-
-function fecharModal(){
-    let msgModal = document.querySelector('.fecharModal')
-
-    msgModal.style.display = 'none'
-}
-
